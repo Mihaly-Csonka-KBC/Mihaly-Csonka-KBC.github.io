34886f06123c6b4f3b1f9761cea4831514b4655b
Mihály Csonka <mihaly.csonka@kbc.be>
2023-06-27 11:22:27 +0200

    Change

--- assets/hashtag.js
+++
@@ -1,3 +0,0 @@
-if (window.location.hash) {
-  window.location.replace(window.location.hash.replace(/#/g, '').replace(/\/\//g, ''));
-}
--- assets/serviceworker.js
+++
@@ -1,5 +0,0 @@
-navigator.serviceWorker?.getRegistrations().then(registrations => {
-  for (let registration of registrations) {
-    registration.unregister();
-  }
-});
